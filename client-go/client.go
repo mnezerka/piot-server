e128a43a96ddb07f7e8a36486a467e27d92d32a6
Michal Nezerka <michal.nezerka@sap.com>
2019-08-09 16:21:35 +0200

    Initial stuff - simple client + mqtt broker config

---
+++ client-go/client.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/url"
+    "os"
+    "time"
+
+    mqtt "github.com/eclipse/paho.mqtt.golang"
+)
+
+func connect(clientId string, uri *url.URL) mqtt.Client {
+    opts := createClientOptions(clientId, uri)
+    client := mqtt.NewClient(opts)
+    token := client.Connect()
+    for !token.WaitTimeout(3 * time.Second) {
+    }
+    if err := token.Error(); err != nil {
+        log.Fatal(err)
+    }
+    return client
+}
+
+func createClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions {
+    opts := mqtt.NewClientOptions()
+    opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
+    opts.SetUsername(uri.User.Username())
+    password, _ := uri.User.Password()
+    opts.SetPassword(password)
+    opts.SetClientID(clientId)
+    return opts
+}
+
+func listen(uri *url.URL, topic string) {
+    client := connect("sub", uri)
+    client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
+        fmt.Printf("<- [%s] %s\n", msg.Topic(), string(msg.Payload()))
+    })
+}
+
+func main() {
+    uri, err := url.Parse(os.Getenv("MQTT"))
+    if err != nil {
+        log.Fatal(err)
+    }
+    fmt.Printf("Broker address - tcp://%s\n", uri.Host)
+
+    topic := "test"
+    topicdev1 := "test/dev1"
+    topicdev2 := "test/dev2"
+
+    // subscribe for messages from my clients
+    go listen(uri, "test/#")
+
+    // subscribe for system messages
+    go listen(uri, "$SYS")
+
+    client := connect("pub", uri)
+    timer := time.NewTicker(5 * time.Second)
+    for t := range timer.C {
+        fmt.Printf("-> [%s], message: %s\n", topic, t.String())
+        client.Publish(topic, 0, false, t.String())
+
+        fmt.Printf("-> [%s]\n", topicdev1)
+        client.Publish(topicdev1, 0, false, "dev1 tick")
+
+        fmt.Printf("-> [%s]\n", topicdev2)
+        client.Publish(topicdev2, 0, false, "dev2 tick")
+    }
+}
---
+++ client-go/go.mod
@@ -0,0 +1,8 @@
+module github.com/mnezerka/piot/client-go
+
+go 1.11
+
+require (
+	github.com/eclipse/paho.mqtt.golang v1.2.0
+	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
+)
