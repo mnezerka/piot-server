d19ccb398ad22080a67576c6f121c2595eedac1e
Michal Nezerka <michal.nezerka@sap.com>
2019-09-13 12:07:45 +0200

    [mgr] Content on home page

--- manager/src/pages/Home.svelte
+++ manager/src/pages/Home.svelte
@@ -1,5 +1,12 @@
-<div class="content">
-
-<p>Welcome to the PIOT Manager</p>
-
-</div>
+<section class="hero is-medium">
+  <div class="hero-body">
+    <div class="container has-text-centered">
+      <h1 class="title">
+        Welcome to PIOT Manager
+      </h1>
+      <h2 class="subtitle">
+        Platform for management of IOT things.
+      </h2>
+    </div>
+  </div>
+</section>
