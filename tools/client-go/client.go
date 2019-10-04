0a277a3199ccab6b9d0e68adb6be61992847ff41
Michal Nezerka <michal.nezerka@sap.com>
2019-10-04 14:05:59 +0200

    Better CLI integration of mqtt tests client

--- tools/client-go/client.go
+++ tools/client-go/client.go
@@ -3,15 +3,24 @@ package main
 import (
     "fmt"
     "log"
-    "net/url"
     "os"
     "time"
-
+    "github.com/urfave/cli"
     mqtt "github.com/eclipse/paho.mqtt.golang"
 )
 
-func connect(clientId string, uri *url.URL) mqtt.Client {
-    opts := createClientOptions(clientId, uri)
+func connect(clientId, uri, username, password string) mqtt.Client {
+
+    opts := mqtt.NewClientOptions()
+    opts.AddBroker(uri)
+    opts.SetClientID(clientId)
+    if username != "" {
+        opts.SetUsername(username)
+        if password != "" {
+            opts.SetPassword(password)
+        }
+    }
+
     client := mqtt.NewClient(opts)
     token := client.Connect()
     for !token.WaitTimeout(3 * time.Second) {
@@ -22,39 +31,45 @@ func connect(clientId string, uri *url.URL) mqtt.Client {
     return client
 }
 
-func createClientOptions(clientId string, uri *url.URL) *mqtt.ClientOptions {
-    opts := mqtt.NewClientOptions()
-    opts.AddBroker(fmt.Sprintf("tcp://%s", uri.Host))
-    opts.SetUsername(uri.User.Username())
-    password, _ := uri.User.Password()
-    opts.SetPassword(password)
-    opts.SetClientID(clientId)
-    return opts
-}
 
+/*
 func listen(uri *url.URL, topic string) {
     client := connect("sub", uri)
     client.Subscribe(topic, 0, func(client mqtt.Client, msg mqtt.Message) {
         fmt.Printf("<- [%s] %s\n", msg.Topic(), string(msg.Payload()))
     })
 }
+*/
 
-func main() {
-    // in format tcp://localhost:1883
-    uri, err := url.Parse(os.Getenv("MQTT"))
-    if err != nil {
-        log.Fatal(err)
-    }
-    fmt.Printf("Broker address - tcp://%s\n", uri.Host)
+func runClient(c *cli.Context) {
 
-    topic := "test"
-    topicdev1 := "test/dev1"
-    topicdev2 := "test/dev2"
+    uri := c.GlobalString("mqtt-uri")
+    topic := c.GlobalString("topic")
+    value := c.GlobalString("value")
+
+    fmt.Printf("MQTT address - %s\n", uri)
+    fmt.Printf("MQTT user: %s\n", c.GlobalString("mqtt-user"))
+    fmt.Printf("MQTT topic: %s\n", topic)
+    fmt.Printf("MQTT value: %s\n", value)
+
+    //topicdev1 := "test/dev1"
+    //topicdev2 := "test/dev2"
 
     // subscribe for messages from my clients
-    go listen(uri, "test/#")
+    //go listen(uri, "test/#")
 
-    client := connect("pub", uri)
+    client := connect("mqtt-client", uri, c.GlobalString("mqtt-user"), c.GlobalString("mqtt-password"))
+
+    defer client.Disconnect(250)
+
+    token := client.Publish(topic, 0, false, value)
+    for !token.WaitTimeout(3 * time.Second) { }
+    if err := token.Error(); err != nil {
+        log.Fatal(err)
+    }
+
+
+    /*
     timer := time.NewTicker(1 * time.Second)
     for t := range timer.C {
         fmt.Printf("-> [%s], message: %s\n", topic, t.String())
@@ -66,4 +81,43 @@ func main() {
         fmt.Printf("-> [%s]\n", topicdev2)
         client.Publish(topicdev2, 0, false, "dev2 tick")
     }
+    */
+}
+
+func main() {
+    app := cli.NewApp()
+
+    app.Name = "MQTT Client"
+    app.Action = runClient
+    app.Flags = []cli.Flag{
+        cli.StringFlag{
+            Name:   "mqtt-uri,q",
+            Usage:  "Endpoint of the MQTT message broker",
+            EnvVar: "MQTT_URI",
+            Value:  "tcp://localhost:1883",
+        },
+        cli.StringFlag{
+            Name:   "mqtt-user",
+            Usage:  "Username for mqtt authentication",
+            EnvVar: "MQTT_USER",
+        },
+        cli.StringFlag{
+            Name:   "mqtt-password",
+            Usage:  "Password for mqtt authentication",
+            EnvVar: "MQTT_PASSWORD",
+        },
+        cli.StringFlag{
+            Name:   "topic",
+            Usage:  "MQTT Topic",
+            EnvVar: "MQTT_TOPIC",
+        },
+        cli.StringFlag{
+            Name:   "value",
+            Usage:  "MQTT Value",
+            EnvVar: "MQTT_VALUE",
+        },
+    }
+
+    app.Run(os.Args)
+
 }
--- tools/client-go/go.mod
+++ tools/client-go/go.mod
@@ -4,5 +4,6 @@ go 1.11
 
 require (
 	github.com/eclipse/paho.mqtt.golang v1.2.0
+	github.com/urfave/cli v1.22.1
 	golang.org/x/net v0.0.0-20190724013045-ca1201d0de80 // indirect
 )
