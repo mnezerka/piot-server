e2aeab1464f1d5937ee9f922523787cf5f895e8b
michaln <michal.nezerka@gmail.com>
2019-10-06 22:47:37 +0200

    [psrv] More details in aux script for things

--- piot-server/scripts/things.sh
+++ piot-server/scripts/things.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-curl -s -X POST http://localhost:9096/query -d '{"query":"{things {id, name, org {id, name}}}"}' -H "$(cat headers.curl)" | jq
+curl -s -X POST http://localhost:9096/query -d '{"query":"{things {id, name, org {id, name} availability_topic}}"}' -H "$(cat headers.curl)" | jq
