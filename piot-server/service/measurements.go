bd306eaa5475735893e59dac6709b79c5cbe5d4a
michaln <michal.nezerka@gmail.com>
2019-10-07 22:04:25 +0200

    [psrv] New service for measurements handling

---
+++ piot-server/service/measurements.go
@@ -0,0 +1,21 @@
+package service
+
+import (
+    /*
+    "context"
+    "fmt"
+    "strings"
+    "time"
+    "github.com/op/go-logging"
+    "piot-server/model"
+    "go.mongodb.org/mongo-driver/bson/primitive"
+    mqtt "github.com/eclipse/paho.mqtt.golang"
+    */
+)
+
+type IMeasurements interface {
+}
+
+type Measurements struct {
+}
+
