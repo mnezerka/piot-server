e93b4dd3d2a78181b26dda76b28af560c7fd73db
michaln <michal.nezerka@gmail.com>
2020-01-03 17:10:24 +0100

    [psrv] Some doc strings

--- piot-server/model/thing.go
+++ piot-server/model/thing.go
@@ -13,15 +13,34 @@ const THING_CLASS_PRESSURE = "pressure"
 
 // Represents any device or app
 type Thing struct {
+    // unique id of the thing
     Id          primitive.ObjectID `json:"id" bson:"_id,omitempty"`
+
+    // name of the thing
     Name        string `json:"name" bson:"name"`
+
+    // optional alias of the thing - used e.g. in graphs
     Alias       string `json:"alias" bson:"alias"`
+
+    // basic classification of things - device vs sensor
     Type        string `json:"type" bson:"type"`
+
+    // is thing enabled?
     Enabled     bool   `json:"enabled" bson:"enabled"`
+
+    // date of thing creation
     Created     int32  `json:"created" bson:"created"`
+
+    // id of the organization thing belongs to
     OrgId       primitive.ObjectID `json:"org_id" bson:"org_id"`
+
+    // ?
     Org         *Org `json:"org" bson:"org"`
+
+    // is thing available
     Available   bool   `json:"available" bson:"available"`
+
+    // time the thing was seen last time
     LastSeen    int32  `json:"last_seen" bson:"last_seen"`
 
     // The MQTT topic subscribed to receive thing availability
@@ -35,8 +54,10 @@ type Thing struct {
     Sensor SensorData `json:"sensor bson:"sensor""`
 }
 
+// Represents measurements for things that are sensors
 type SensorData struct {
-    // The MQTT topic subscribed to receive sensor values.
+
+    // The MQTT topic where sensor values are published
     MeasurementTopic string `json:"measurement_topic" bson:"measurement_topic"`
 
     // Time when last measurement was received
@@ -45,7 +66,7 @@ type SensorData struct {
     // Type of sensor measurement
     Class string `json:"class" bson:"class"`
 
-    // Defines the number of seconds sinc last measurement for which the
+    // Defines the number of seconds since last measurement for which the
     // measurement is valid
     Validity int32  `json:"validity" bson:"validity"`
 
