da8e3bdd9a8fe25530948fe5eb69b6ecadce10be
mnezerka <michal.nezerka@sap.com>
2020-02-04 16:26:56 +0100

    Adaptation on macOS dev environment

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
 
-MONGO_ADDR=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' piot-server_mongodb_1)
+#MONGO_ADDR=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' piot-server_mongodb_1)
+MONGO_ADDR=localhost
 echo Setting mongodb address to $MONGO_ADDR
 export MONGODB_URI=mongodb://$MONGO_ADDR:27017
 
-PIOT_SERVER_ADDR=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' piot_piot-server_1)
-echo Setting piot server hostname to $PIOT_SERVER_ADDR
-export PIOT_SERVER_HOSTNAME=$PIOT_SERVER_ADDR
+#PIOT_SERVER_ADDR=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' piot_piot-server_1)
+#echo Setting piot server hostname to $PIOT_SERVER_ADDR
+#export PIOT_SERVER_HOSTNAME=$PIOT_SERVER_ADDR
 
 
 
